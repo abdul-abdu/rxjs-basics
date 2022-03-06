f66d781449b8eab3a71845eab3375d8dc6f8cf6f
Abdug'affor Abdurahimov <abdurahimov.97@list.ru>
2022-03-06 22:22:43 +0500

    First commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-import { fromEvent } from "rxjs"
+import { fromEvent, map, mapTo, pluck } from "rxjs"
 
 const keyup$ = fromEvent(document, "keyup")
+// const keyKode$ = keyup$.pipe(map((event: KeyboardEvent) => event.code))
+// const keyKodeWithPluck$ = keyup$.pipe(pluck("code"))
 
-console.log(12);
+const pressed$ = keyup$.pipe(
+  mapTo("Key Pressed")
+)
 
+pressed$.subscribe(console.log)
